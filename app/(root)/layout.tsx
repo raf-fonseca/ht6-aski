ecd6da1d41b7d85449d2b24df39833680f859768
victoriamazilu <victoria.mazilu@icloud.com>
2024-08-03 11:41:27 -0400

    Some more setup

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "Aski",
+    description: "Feeling indecisive? Don't fear, Aski's here!",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
+}
---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <main className="sm:mx-24 mx-0">
+            <div className="">Hello world! Welcome to Aski</div>
+        </main>
+    );
+}
