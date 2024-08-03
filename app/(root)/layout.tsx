2ed6784f422adf5113c081319f3fb5d9173ab5e8
victoriamazilu <victoria.mazilu@icloud.com>
2024-08-03 15:54:19 -0400

    Implement bottom bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "../globals.css";
 import Topbar from "@/components/shared/Topbar";
 import { Sidebar } from "@/components/shared/Sidebar";
+import Bottombar from "@/components/shared/Bottombar";
 
 const inter = Inter({
     subsets: ["latin"],
@@ -28,6 +29,7 @@ export default function RootLayout({
                         <div className="w-full max-w-4xl">{children}</div>
                     </section>
                 </main>
+                <Bottombar />
             </body>
         </html>
     );
