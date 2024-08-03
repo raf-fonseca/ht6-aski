eb0ca07e04910be7619aec44a4eb5d8d506b6115
Rafael Fonseca <rafael.silvanofonseca@gmail.com>
2024-08-03 09:46:12 -0400

    Initializing project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="sm:mx-24 mx-0">
+      <div className="">
+        hello
+      </div>
+    </main>
+  );
+}
